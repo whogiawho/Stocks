00053ba7ba85966769c7607e85cb7441555654cf
whogiawho <whogiawho@gmail.com>
2020-01-13 18:54:49 +0800

    AStockSdTime 1st range is got from settings.txt's SdStartTime

--- java/com/westsword/stocks/base/time/AStockSdTime.java
+++ java/com/westsword/stocks/base/time/AStockSdTime.java
@@ -1,11 +1,22 @@
 package com.westsword.stocks.base.time;
 
 
+import com.westsword.stocks.Settings;
+
 public class AStockSdTime extends SdTime {
     public AStockSdTime() {
-        super();
+        this(Settings.getSdInterval());
+    }
+    public AStockSdTime(int interval) {
+        super(interval);
 
-        addRange("09:25:00", "09:25:00");
+        addRanges();
+    }
+
+    private void addRanges() {
+        String sdStartTime = Settings.getSdStartTime();   //09:25:00
+
+        addRange(sdStartTime, sdStartTime);
         addRange("09:30:00", "11:30:00");
         addRange("13:00:00", "15:00:00");
     }
