64c39d0bd565e362dd6b47b824c7566a629b695f
whogiawho <whogiawho@gmail.com>
2020-02-04 22:17:49 +0800

    Update StockPaths.java
    several paths added
    1. \data\similarStack
    2. \data\similarStack\[stockCode]
    3. getSSTradeDetailsLogDir
    4. getSSTradeDetailsLogFile

--- java/com/westsword/stocks/utils/StockPaths.java
+++ java/com/westsword/stocks/utils/StockPaths.java
@@ -1,5 +1,6 @@
 package com.westsword.stocks.utils;
 
+import com.westsword.stocks.Stock;
 import com.westsword.stocks.Settings;
 
 public class StockPaths {
@@ -32,6 +33,40 @@ public class StockPaths {
 
 
     
+    public static String getSSRootDir() {
+        return getStockRootDir() + "\\data\\similarStack\\";
+    }
+    public static String getSimilarStackDir(String stockCode) {
+        return getSSRootDir() + stockCode + "\\";
+    }
+    public static String getSSTradeDetailsLogDir(String stockCode, String startDate, 
+            double threshold, int tradeType, int sTDistance,
+            String tradeDate, int maxCycle, double targetRate) {
+        String sDir = "";
+        sDir += getSimilarStackDir(stockCode) + startDate + "_" + String.format("%.2f", threshold);
+        sDir += "_" + "T" + sTDistance + getTradeTypeSymbol(tradeType) + "\\";
+
+        sDir += tradeDate + "_" + String.format("%03d_%.3f", maxCycle, targetRate);
+        sDir += "\\";
+
+        return sDir;
+    }
+    public static String getSSTradeDetailsLogFile(String stockCode, String startDate, 
+            double threshold, int tradeType, int sTDistance,
+            String tradeDate, String hmsList, int maxCycle, double targetRate) {
+        String sDir = getSSTradeDetailsLogDir(stockCode, startDate,
+                threshold, tradeType, sTDistance,
+                tradeDate, maxCycle, targetRate);
+
+        return sDir + hmsList + ".txt";
+    }
+    private static String getTradeTypeSymbol(int tradeType) {
+        String sTradeType = "L";
+        if(tradeType == Stock.TRADE_TYPE_SHORT)
+            sTradeType = "S";
+
+        return sTradeType;
+    }
 
 
 
