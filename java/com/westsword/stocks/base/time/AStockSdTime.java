db7107cf739850e3d05effa27689d88b47288fa6
whogiawho <whogiawho@gmail.com>
2020-02-04 22:18:32 +0800

    Update AStockSdTime.java
    getCallAuctionEndTime
    getClosQuotationTime

--- java/com/westsword/stocks/base/time/AStockSdTime.java
+++ java/com/westsword/stocks/base/time/AStockSdTime.java
@@ -19,10 +19,17 @@ public class AStockSdTime extends SdTime {
         addRange("13:00:00", "15:00:00");
     }
 
-    public String getCallAuctionEndTime0() {
+    public static String getCallAuctionEndTime0() {
         return Settings.getSdStartTime();           //09:25:00
     }
-    public String getCallAuctionEndTime1() {
+    public static String getCallAuctionEndTime1() {
         return "09:25:02";
+    }
+
+    public static String getCallAuctionEndTime() {
+        return "09:25:00"; 
+    }
+    public static String getCloseQuotationTime() {
+        return "15:00:00";
     }
 }
