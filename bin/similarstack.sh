52d309437f7a43c2b5b7d6ca8b6d6a10bbc97b97
whogiawho <whogiawho@gmail.com>
2020-02-17 08:04:16 +0800

    Update similarstack.sh
    1. rename getFullWinList to getSSHMSListFullWin
    2. add getSSHMSList
      list hmsList meeting tradeLength<maxwait
    3. add getSSCommon and baseGetSSCommon
      find the intersection of two groups:
       <tradeDate1, hmsList1>
       <tradeDate2, hmsList2>

--- bin/similarstack.sh
+++ bin/similarstack.sh
@@ -2,7 +2,7 @@
 
 #dir=data/similarStack/600030/20160108_0.90_T1L/20160111_180_1.100
 #maxwait=180
-function getFullWinList {
+function getSSHMSListFullWin {
     local dir=$1
     local maxwait=$2
 
@@ -11,14 +11,11 @@ function getFullWinList {
     for i in `sort -nk3,3 $file |grep "100.*%"|awk '{print $9}'`;     #get those 100% winning hmsList
     do 
         local allg=0; 
-        local j=
-        for j in `awk '{print $12}' $dir/$i.txt`; 
-        do 
-            [[ $j -ge $maxwait ]] && { 
-                allg=1; 
-                break; 
-            }; 
-        done; 
+        local maxPeriod=`awk '{print $12}' $dir/$i.txt|sort -n|tail -n 1`
+
+        [[ $maxPeriod -ge $maxwait ]] && { 
+            allg=1; 
+        }
         [[ $allg == 0 ]] && { 
             local c=`wc $dir/$i.txt|awk '{print $1}'`;                      #matchedTradeDates
             local m1=`awk '{print $12}' $dir/$i.txt|sort -nu|tail -n 1`;    #max tradeLength
@@ -28,4 +25,59 @@ function getFullWinList {
     done
 }
 
+function getSSHMSList {
+    local dir=$1
+    local maxwait=$2
 
+    local file=$dir.txt; 
+    local i=
+    for i in `awk '{print $9}' $file`; 
+    do 
+        local count=`awk "\\$12<=$maxwait{print \\$1}" $dir/$i.txt|wc|awk '{print $1}'`;
+        echo $i $count; 
+    done
+}
+
+function getSSCommon {
+    local dir1=$1
+    local maxwait=$2
+    local dir2=$3
+
+    local file1=$dir1.txt; 
+    local list1=`awk '{print $9}' $file1`
+
+    local file2=$dir2.txt; 
+    local list2=`awk '{print $9}' $file2`
+
+    local i=
+    local j=
+    for i in $list1
+    do
+        for j in $list2
+        do
+            baseGetSSCommon $dir1 $i $maxwait $dir2 $j
+        done
+    done
+}
+
+function baseGetSSCommon {
+    local dir1=$1
+    local hmsList1=$2
+    local maxwait=$3
+    local dir2=$4
+    local hmsList2=$5
+
+
+    local fTmp1=`mktemp`
+    local file1=$dir1/$hmsList1.txt
+    awk "\$12<=$maxwait{print \$1}" $file1 >$fTmp1
+
+    local fTmp2=`mktemp`
+    local file2=$dir2/$hmsList2.txt
+    awk "{print \$1}" $file2 >$fTmp2
+
+    local count=`comm -12 $fTmp1 $fTmp2|wc|awk '{print $1}'`
+    echo $dir1 $hmsList1 $dir2 $hmsList2 $count
+
+    rm -rf $fTmp1
+}
