ce13f1ab8f7db57d6e7f40c268d1e82f9cfc0fb1
whogiawho <whogiawho@gmail.com>
2020-04-13 01:41:46 +0800

    Update CheckPoint0.java
    remove 09:30:00 from CheckPoint0

--- java/com/westsword/stocks/base/ckpt/CheckPoint0.java
+++ java/com/westsword/stocks/base/ckpt/CheckPoint0.java
@@ -17,12 +17,13 @@ public class CheckPoint0 extends CheckPoint {
         //add 09:25:00
         add(AStockSdTime.getCallAuctionEndTime0());
 
-        //add 09:30:00, 11:30:00
-        long startTp = Time.getSpecificTime(currentDate, AStockSdTime.getOpenQuotationTime());
+        //skip 09:30:00
+        //add [09:31:00, 11:30:00]
+        long startTp = Time.getSpecificTime(currentDate, AStockSdTime.getOpenQuotationTime()) + interval;
         long endTp = Time.getSpecificTime(currentDate, AStockSdTime.getMidSuspensionTime());
         add(startTp, endTp, interval);
 
-        //add 13:00:00, endHMS
+        //add [13:00:00, endHMS]
         startTp = Time.getSpecificTime(currentDate, AStockSdTime.getMidOpenQuotationTime());
         endTp = Time.getSpecificTime(currentDate, endHMS);
         add(startTp, endTp, interval);
