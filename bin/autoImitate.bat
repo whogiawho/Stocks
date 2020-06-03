d9c930bc894568a08be3fab177eb3c8b532fb97e
whogiawho <whogiawho@gmail.com>
2020-06-03 19:32:28 +0800

    remove settings.py

--- bin/autoImitate.bat
+++ bin/autoImitate.bat
@@ -3,4 +3,4 @@ REM @echo off
 call stockEnv.bat
 
 cd %rootDir%\bin
-imitateRealtimeData.bat ..\data\copyList\600030.20200601.copylist.txt
+imitateRealtimeData.bat ..\data\copyList\600030.20200603.copylist.txt
