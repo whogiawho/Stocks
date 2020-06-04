937789492783f71583188984c2bccf7659e58605
whogiawho <whogiawho@gmail.com>
2020-06-04 16:11:58 +0800

    update gnuv3 prefix to java file

--- java/com/westsword/stocks/tools/helper/CmManager.java
+++ java/com/westsword/stocks/tools/helper/CmManager.java
@@ -1,5 +1,5 @@
  /*
- Copyright (C) 1989-2020 Free Software Foundation, Inc.
+ Copyright (C) 2019-2050 WestSword, Inc.
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
